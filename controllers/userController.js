e46ac3e7ab54585435094021d3605bf1444ab9d1
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-10-14 21:54:39 +0530

    login api done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
 import passwordHash from 'password-hash'
 import User from "../models/user.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
 
+dotenv.config();
 
 export function save(req, res) {
     req.body.password = passwordHash.generate(req.body.password); // password hashing
@@ -16,4 +19,30 @@ export function save(req, res) {
             error: err
         })
     });
+}
+
+export function login(req, res) {
+    User.findOne({ email: req.body.email }).then((user) => {
+        if (user || !user.disabled) {
+            if(passwordHash.verify(req.body.password, user.password)){ // check password
+                const payload = {
+                    email: user.email,
+                    password: user.password,
+                    type: user.type
+                }
+                const token = jwt.sign(payload, process.env.JWT_KEY) // generate token
+                res.json({
+                    message: "Login success",
+                    token: token
+                })
+            }
+            else res.json({message: "Password is wrong"});
+        }
+        else res.json({ message: "User not found" });
+    }).catch((err) => {
+        res.json({
+            message: "Logging fail",
+            error: err
+        })
+    })
 }
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { save } from "../controllers/userController.js";
+import { login, save } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/", save)
+userRouter.post("/", save);
+
+userRouter.post("/login", login);
 
 export default userRouter;
