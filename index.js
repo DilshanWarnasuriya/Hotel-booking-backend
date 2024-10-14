637bd0b2676563da35fb5f556b991bb10446dd12
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-10-14 20:14:02 +0530

    database connection done

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import bodyParser from "body-parser";
 import express from "express";
 import dotenv from 'dotenv'
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+//database connection
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("Database Connected")).catch(() => console.log("database not connected"));
 
 app.listen(5000, (req, res) => {
     console.log("Application start in port 5000");
