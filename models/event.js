02ecb2841f01ce7c2d5f16eb9b84369d2d6f8953
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-10-15 13:27:24 +0530

    event model done

---
+++ models/event.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const eventSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+})
+
+export default mongoose.model("event", eventSchema);
