3f1033ca2eb7f7c866a90c057f9df92842263ada
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-10-18 22:28:09 +0530

    booking cancel api done

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,18 +1,18 @@
 import Booking from "../models/booking.js";
 import Room from "../models/room.js";
-import { isUser } from "./userController.js";
+import { isAdmin, isUser } from "./userController.js";
 
 export async function create(req, res) {
     if (isUser(req)) {
         if (compareDates(req.body.endDate, req.body.startDate)) { // Check new booking has end date after the start date
 
             const lastBooking = await Booking.findOne({ roomNo: req.body.roomNo }).sort({ endDate: -1 }).limit(1); // Get last booking of the using room id
-            const room = await Room.findOne({number: req.body.roomNo});
+            const room = await Room.findOne({ number: req.body.roomNo });
 
             if (lastBooking && compareDates(lastBooking.endDate, req.body.startDate) && lastBooking.status != "cancel") { // Check room is booked before and if the new booking start date is before the last booking end date.
                 res.json({ message: "Sorry!. Can't book this room right now" })
             }
-            else if(req.body.personCount > room.maxPerson){
+            else if (req.body.personCount > room.maxPerson) {
                 res.json({ message: "Sorry!. Can't book this room. because a room has more than the maximum person count. Please see a bigger room" })
             }
             else {
@@ -38,14 +38,24 @@ export async function create(req, res) {
 }
 
 export function getAll(req, res) {
-    Booking.find().then((result) => {
-        res.json(result)
-    }).catch(() => {
-        res.json({ message: "Server error" })
-    });
+    if (isAdmin(req)) {
+        Booking.find().then((result) => {
+            res.json(result)
+        }).catch(() => {
+            res.json({ message: "Server error" })
+        });
+    } else res.json({ message: "not permission" });
 }
 
-
+export function cancel(req, res) {
+    if (isUser(req)) {
+        Booking.updateOne({ id: req.params.id }, { status: "cancel" }).then(() => {
+            res.json({ message: "Booking cancel success" })
+        }).catch(() => {
+            res.json({ message: "Booking cancel fail" })
+        });
+    } else res.json({ message: "not permission" });
+}
 
 function compareDates(date1, date2) {
     const d1 = new Date(date1);
--- routers/bookingRouter.js
+++ routers/bookingRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { create, getAll } from '../controllers/bookingController.js';
+import { cancel, create, getAll } from '../controllers/bookingController.js';
 
 const bookingRouter = express.Router();
 
 bookingRouter.post("/", create);
 
 bookingRouter.get("/", getAll);
+
+bookingRouter.put("/cancel/:id", cancel);
 
 export default bookingRouter;
