49dbc4e4b9c59bca86954f7312aa217594fd5452
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-10-18 19:15:15 +0530

    booking model done

---
+++ models/booking.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const bookingSchema = mongoose.Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    roomNo: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    personCount: {
+        type: Number,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        default: "pending"
+    },
+    reason: {
+        type: String,
+        default: ""
+    },
+    notes: {
+        type: String,
+        default: ""
+    },
+    timeStamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default mongoose.model("Booking", bookingSchema);
