924391eb7b4d48d8887ea961144291b095621ba2
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-10-18 21:57:56 +0530

    add new condition to create booking api

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,4 +1,5 @@
 import Booking from "../models/booking.js";
+import Room from "../models/room.js";
 import { isUser } from "./userController.js";
 
 export async function create(req, res) {
@@ -6,9 +7,13 @@ export async function create(req, res) {
         if (compareDates(req.body.endDate, req.body.startDate)) { // Check new booking has end date after the start date
 
             const lastBooking = await Booking.findOne({ roomNo: req.body.roomNo }).sort({ endDate: -1 }).limit(1); // Get last booking of the using room id
+            const room = await Room.findOne({number: req.body.roomNo});
 
             if (lastBooking && compareDates(lastBooking.endDate, req.body.startDate) && lastBooking.status != "cancel") { // Check room is booked before and if the new booking start date is before the last booking end date.
                 res.json({ message: "Sorry!. Can't book this room right now" })
+            }
+            else if(req.body.personCount > room.maxPerson){
+                res.json({ message: "Sorry!. Can't book this room. because a room has more than the maximum person count. Please see a bigger room" })
             }
             else {
                 req.body.id = await Booking.countDocuments() + 1;
