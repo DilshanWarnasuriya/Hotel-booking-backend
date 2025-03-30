9fc8fe80994b8631333e5705f8295581a3471b61
yashdilshan <yashodadilshan@gmail.com>
2025-03-30 11:38:00 +0530

    Set Remove api router path

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { findByContactNo, findById, login, persist, retrieve, update } from "../controllers/userController.js";
+import { findByContactNo, findById, login, persist, remove, retrieve, update } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
@@ -14,5 +14,7 @@ userRouter.get("/contactNo/:contactNo", findByContactNo);
 userRouter.get("/id/:id", findById);
 
 userRouter.put("/", update);
+
+userRouter.delete("/:id", remove);
 
 export default userRouter;
